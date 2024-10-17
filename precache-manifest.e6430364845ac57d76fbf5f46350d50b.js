b6593e06a34be19478b19ce387368544d7646fa7
Kiki <ismajlikiki@gmail.com>
2024-10-17 11:50:19 -0500

    Updates

---
+++ precache-manifest.e6430364845ac57d76fbf5f46350d50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46e1cf2a8f854354585abc80569c1be",
+    "url": "/crypto-table/index.html"
+  },
+  {
+    "revision": "01d3db8f57f128ecf770",
+    "url": "/crypto-table/static/css/main.66e55bee.chunk.css"
+  },
+  {
+    "revision": "08e19fe8ade47bed1e1b",
+    "url": "/crypto-table/static/js/2.019ae9ac.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/crypto-table/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "01d3db8f57f128ecf770",
+    "url": "/crypto-table/static/js/main.f2eda056.chunk.js"
+  },
+  {
+    "revision": "acfbc15da6672b4613ca",
+    "url": "/crypto-table/static/js/runtime~main.b7e52439.js"
+  }
+]);
